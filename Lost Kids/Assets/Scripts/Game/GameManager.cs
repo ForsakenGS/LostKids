c7d76e498d9657b6847feba0bce609e1fc438269
David <bellolinares@yahoo.es>
2016-01-26 13:40:32 +0100

    Creación de escenas (menu, options, credits) y del script GameManager para navegar por dichas pantallas. En escena menu insertados botones enlazados a sus correspondientes escenas (además del botón exit)y además del botón Load Game el cual de momento se queda sin enlazar. Todo muy básico a falta de incorporar arte. David.

---
+++ Lost Kids/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ExitApplication (){
+		Application.Quit ();
+	}
+
+	public void GoToScene(string sc) {
+		Application.LoadLevel (sc);
+	}
+}
+
+
+
+	
