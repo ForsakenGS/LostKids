2ce7b01e67460c0ae888719cc083d49f096136d3
danymad92 <danymad92@gmail.com>
2016-02-22 13:56:59 +0100

    Message Manager, MessageTrigger y MessageTriggerEditor añadidos

---
+++ Lost Kids/Assets/Scripts/Messages/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MessageManager : MonoBehaviour {
+
+    private ArrayList messages;
+
+    public TextAsset messageFile;
+
+	// Use this for initialization
+	void Start () {
+        messages = new ArrayList();
+
+        FillMessages();
+	}
+	
+    private void FillMessages() {
+
+        string fullText = messageFile.text;
+
+        string[] formatText = fullText.Split(';');
+
+        for(int i = 0; i < formatText.Length; i++) {
+
+            messages.Add((string)formatText[i]);
+
+        }
+
+    }
+
+
+    public void ShowMessage(int index) {
+        Debug.Log(messages[index]);
+
+        string msg = (string)messages[index];
+
+        string[] lines = msg.Split('@');
+
+        Debug.Log(lines.Length);
+
+        //BroadcastMessage("Lock");
+    }
+
+
+}
