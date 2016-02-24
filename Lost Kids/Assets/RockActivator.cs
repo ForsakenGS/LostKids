0ac13419189b298c6064ae95900b483605cc8a96
David <bellolinares@yahoo.es>
2016-02-24 18:46:24 +0100

    primera parte del montaje del nivel 1 - David

---
+++ Lost Kids/Assets/RockActivator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockActivator : MonoBehaviour {
+	public GameObject wall;
+	public GameObject rock;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			rock.GetComponent<KillerSphere> ().enabled = true;
+			wall.GetComponent<MeshRenderer> ().enabled = false;
+		}}
+}
---
+++ Lost Kids/Assets/Scripts/Enemies/KillerSphere.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillerSphere : MonoBehaviour {
+	public int force;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddForce (force * transform.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+}
+
