444cae2753bb5f4ec084ee92b27c261c217f3241
danymad92 <danymad92@gmail.com>
2016-04-18 21:21:17 +0200

    Inicio del PCG con etiquetas

---
+++ Lost Kids/Assets/Scripts/EnumFlags.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EnumFlags : PropertyAttribute {
+
+     public EnumFlags() { }
+
+    [CustomPropertyDrawer(typeof(EnumFlags))]
+    public class EnumFlagsDrawer : PropertyDrawer   {
+
+        public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label) {
+            _property.intValue = EditorGUI.MaskField(_position, _label, _property.intValue, _property.enumNames);
+        }
+    }
+}
