4e3ac039b9c0af139356b8f92077eb3f315cb0cd
JuanCarlosGdM <juancarlosgdm@gmail.com>
2016-04-26 11:15:48 +0200

    - MessageManager modificado para permitir conversaciones entre distintos personajes - HUD modificado para mejorar visualización mensajes - Kodama implementado

--- Lost Kids/Assets/Scripts/Messages/MessageTrigger.cs
+++ Lost Kids/Assets/Scripts/Messages/MessageTrigger.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class MessageTrigger : MonoBehaviour {
-
-    private MessageManager messageManager;
-
     /// <summary>
     /// Tipo de salto de mensajes:
     /// Unique: Se muestra una vez y no se vuelve a mostrar
@@ -14,41 +11,58 @@ public class MessageTrigger : MonoBehaviour {
     public enum MessageChecker { Unique, Always, ShowTime }
     public MessageChecker type;
 
-    //Tiempo para que vuelva a activarse el trigger
+    /// <summary>
+    /// Listado con índices de los mensajes a mostrar
+    /// </summary>
+    public List<int> indexList;
+    /// <summary>
+    /// Indica si se cumplen las condiciones para que los mensajes puedan ser mostrados
+    /// </summary>
+    public bool messagesCanBeShown = true;
+    /// <summary>
+    /// Tiempo hasta que vuelva a activarse el trigger
+    /// </summary>
     public float timeToShowAgain = 0;
 
-    //Tiempo desde que se ha mostrado el último mensaje del trigger
-    private float timeSinceShown = float.MaxValue;
-
-    //Indice del trigger para acceder al mensaje
-    public int index;
-
-    //Indica si se ha mostrado ya el mensaje
+    
+    // Referencia a MessageManager
+    private MessageManager messageManager;
+    // Indica si se ha mostrado ya el mensaje
     private bool messageShown;
+    // Tiempo desde que se ha mostrado el último mensaje del trigger
+    private float timeSinceShown;
+
+    // Use this for references
+    void Awake() {
+        messageManager = GameObject.FindGameObjectWithTag("MessageManager").GetComponent<MessageManager>();
+    }
 
     // Use this for initialization
-    void Start () {
-        messageManager = GameObject.FindGameObjectWithTag("MessageManager").GetComponent<MessageManager>();
+    void Start() {
         messageShown = false;
+        timeSinceShown = float.MaxValue;
     }
-	
+
     void OnTriggerEnter(Collider other) {
-        //Si se trata del jugador activo
-        if(CharacterManager.IsActiveCharacter(other.gameObject)) {
-            //Si el tipo de trigger es único y no se ha mostrado el mensaje se llama a mostrar mensaje
-            if(type.Equals(MessageChecker.Unique) && !messageShown) {
-                messageShown = true;
-                messageManager.ShowMessage(index);
-            //Si no, si el tipo de trigger es por tiempo y el tiempo del trigger ha pasado se llama a mostrar mensaje y se actualiza el tiempo
-            }else if(type.Equals(MessageChecker.ShowTime) && (Time.time - timeSinceShown >= timeToShowAgain) || !messageShown) {
-                messageShown = true;
-                timeSinceShown = Time.time;
-                messageManager.ShowMessage(index);
-            //Si no, si el tipo de trigger es infinito, se muestra el mensaje
-            }else if(type.Equals(MessageChecker.Always)){
-                messageManager.ShowMessage(index);
-            }
+        // Muestra el mensaje si no requiere el botón de acción y se trata del jugador activo
+        if ((messagesCanBeShown) && (CharacterManager.IsActiveCharacter(other.gameObject))) {
+            ShowMessages();
         }
     }
 
+    void ShowMessages() {
+        // Si el tipo de trigger es único y no se ha mostrado el mensaje se llama a mostrar mensaje
+        if (type.Equals(MessageChecker.Unique) && !messageShown) {
+            messageShown = true;
+            messageManager.ShowConversation(indexList);
+            // Si no, si el tipo de trigger es por tiempo y el tiempo del trigger ha pasado se llama a mostrar mensaje y se actualiza el tiempo
+        } else if (type.Equals(MessageChecker.ShowTime) && (Time.time - timeSinceShown >= timeToShowAgain) || !messageShown) {
+            messageShown = true;
+            timeSinceShown = Time.time;
+            messageManager.ShowConversation(indexList);
+            // Si no, si el tipo de trigger es infinito, se muestra el mensaje
+        } else if (type.Equals(MessageChecker.Always)) {
+            messageManager.ShowConversation(indexList);
+        }
+    }
 }
