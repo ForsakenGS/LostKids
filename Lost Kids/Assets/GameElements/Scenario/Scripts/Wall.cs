c6e89dd0b6293b884adaebf8a41152b559a8f445
David <bellolinares@yahoo.es>
2016-05-01 13:23:23 +0200

    Wall.cs y prefab WallConstructor

---
+++ Lost Kids/Assets/GameElements/Scenario/Scripts/Wall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+	public int width;
+	public int height;
+	public int rowNumber;
+	public int columNumber;
+
+	public GameObject[] brick;
+	private GameObject brickSelected;
+
+		void Start() {
+			//for (int i = 0; i < rowNumber; i++) {
+				for (int j = 0; j < columNumber; j++) {
+				float x = transform.position.x + j * width;
+				float y = transform.position.y;
+				float z = transform.position.z;
+			Instantiate(brick[Random.Range(0,brick.Length)], new Vector3(x, y, z), Quaternion.identity);
+			//}
+		}
+	}
+}
