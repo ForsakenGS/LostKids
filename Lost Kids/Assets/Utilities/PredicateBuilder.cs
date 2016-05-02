b8ca8dbe36e951f660867482633d771667ad227b
Eduardo_Vazquez <eduardo.vazquezn@gmail.com>
2016-05-02 22:35:18 +0200

    Im`lementado script generador de niveles

---
+++ Lost Kids/Assets/Utilities/PredicateBuilder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class PredicateBuilder
+{
+    public static Func<T, bool> True<T>() { return f => true; }
+    public static Func<T, bool> False<T>() { return f => false; }
+
+    public static Func<T, bool> Or<T>(this Func<T, bool> expr1,
+                                       Func<T, bool> expr2)
+    {
+        return t => expr1(t) || expr2(t);
+    }
+
+    public static Func<T, bool> And<T>(this Func<T, bool> expr1,
+                                       Func<T, bool> expr2)
+    {
+        return t => expr1(t) && expr2(t);
+    }
+}
