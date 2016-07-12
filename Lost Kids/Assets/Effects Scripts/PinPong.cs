6c46a5cec8e3b24502a2658d7b182ee520ad8a02
danymad92 <danymad92@gmail.com>
2016-07-12 20:49:54 +0200

    Commit de la muerte Cambios en la estética de los niveles. Paredes de distinto color en tutoriales Niebla añadida Mejorado el parallax Intro V2

---
+++ Lost Kids/Assets/Effects Scripts/PinPong.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinPong : MonoBehaviour {
+
+    public float floatSpeed = 0.1f;
+    public float floatDistance = 1;
+    private Vector3 basePosition;
+
+	// Use this for initialization
+	void Awake () {
+	    basePosition = transform.position;
+	}
+
+    void OnEnable()
+    {
+        MoveUp();
+    }
+
+    void OnDisable()
+    {
+        transform.position = basePosition;
+        iTween.Stop(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void MoveUp()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("y", transform.position.y + floatDistance, "speed", floatSpeed,
+            "easeType", iTween.EaseType.easeInOutSine, "oncomplete", "MoveDown", "islocal", true));
+    }
+
+    void MoveDown()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("y", basePosition.y, "speed", floatSpeed,
+            "easeType", iTween.EaseType.easeInOutSine, "oncomplete", "MoveUp", "islocal", true));
+    }
+}
--- Lost Kids/Assets/GameElements/Effects/Scripts/ParallaxCatcher.cs
+++ Lost Kids/Assets/GameElements/Effects/Scripts/ParallaxCatcher.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 public class ParallaxCatcher : MonoBehaviour {
 
-    public Transform player;
+    public Transform target;
 
     private Vector3 relLightPos;
 	// Use this for initialization
 	void Start () {
 	    
-        relLightPos = transform.position - player.position;
+        relLightPos = transform.position - target.position;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         //transform.position = player.position + relLightPos;
-        transform.position = new Vector3(player.position.x + relLightPos.x,transform.position.y,transform.position.z);
+        transform.position = new Vector3(target.position.x + relLightPos.x,transform.position.y,transform.position.z);
 	}
 }
