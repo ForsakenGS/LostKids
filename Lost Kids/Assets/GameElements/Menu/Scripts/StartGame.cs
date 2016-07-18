7f48dc78a129a84fe2b624402ff2758a083b5d31
JuanCarlosGdM <juancarlosgdm@gmail.com>
2016-07-18 20:07:43 +0200

    Resueltos conflictos de escena y problema en la selección de idioma

--- Lost Kids/Assets/GameElements/Menu/Scripts/StartGame.cs
+++ Lost Kids/Assets/GameElements/Menu/Scripts/StartGame.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    
+    // Update is called once per frame
 	void Update () {
 	    if(Input.anyKeyDown) {
             NextScene();
