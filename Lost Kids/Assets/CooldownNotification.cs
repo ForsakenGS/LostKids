0b8328412d59c3b788c0e81441dfc1c9871f1b3f
danymad92 <danymad92@gmail.com>
2016-08-31 17:46:10 +0200

    Añadido Neko Modificado el tiempo en la notificacion cooldown Añadidas nuevas paredes Level1 Añadidos ways y luces provisionales para los niños Añadidos los nuevos Kunais

--- Lost Kids/Assets/CooldownNotification.cs
+++ Lost Kids/Assets/CooldownNotification.cs
@@ -4,6 +4,10 @@ using UnityEngine.UI;
 
 public class CooldownNotification : MonoBehaviour {
 
+    public float duration = 0.5f;
+    public float maxAlpha = 0.4f;
+    public float fadeTime = 0.4f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,12 +23,12 @@ public class CooldownNotification : MonoBehaviour {
         StopAllCoroutines();
         GetComponent<Image>().sprite = spr;
         GetComponent<Image>().color = new Color(1,1,1,0);
-        Invoke("HideNotification", 0.8f);
-        iTween.FadeTo(gameObject, 0.4f, 0.5f);
+        Invoke("HideNotification", duration);
+        iTween.FadeTo(gameObject, maxAlpha, fadeTime);
         
     }
 
     public void HideNotification() {
-       iTween.FadeTo(gameObject, 0, 0.5f);
+       iTween.FadeTo(gameObject, 0, fadeTime);
     }
 }
