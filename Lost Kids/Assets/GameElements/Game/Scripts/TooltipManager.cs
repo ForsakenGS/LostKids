73101529630a219915064cb68989e1ed74f990d5
JuanCarlosGdM <juancarlosgdm@gmail.com>
2016-09-22 20:26:40 +0200

    - Permitir desactivar tooltips desde menú - Integración nuevas animaciones

---
+++ Lost Kids/Assets/GameElements/Game/Scripts/TooltipManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TooltipManager {
+    // Evento para advertir del cambio en el TooltipManager
+    public delegate void TooltipManagerChanged (bool on);
+    public static TooltipManagerChanged TooltipOnOff;
+    public static bool On {
+        get {
+            return _on;
+        }
+        set {
+            _on = value;
+            TooltipOnOff(_on);
+        }
+    }
+    private static bool _on = true;
+}
